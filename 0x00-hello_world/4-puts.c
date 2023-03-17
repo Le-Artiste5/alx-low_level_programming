5e5dcf026ed6ea7d42e62210f52a6e402572a370
Le-Artiste5 <johnsonotobong2@gmail.com>
2023-03-17 14:06:29 -0700

    My first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main:A program that putsa a line with the puts function
- *Return: 0(Successful if it returns to zero)
+ *main-A program that puts a line with puts function)
+ *Return: 0(Success if it returns to zero)
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
