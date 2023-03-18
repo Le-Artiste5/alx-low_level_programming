2ede0838c2bc17530e6353a5659bc8490d6f453b
Le-Artiste5 <johnsonotobong2@gmail.com>
2023-03-18 08:52:10 -0700

    Coding with C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- *main - A program that prints a  line with puts function
+ *main-A program that puts a line with puts function
  *Return: 0(SUCCESS)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
+}
