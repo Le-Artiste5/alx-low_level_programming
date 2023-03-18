6384932b237e0161c72eeff56df6f1e91c10b09d
Le-Artiste5 <johnsonotobong2@gmail.com>
2023-03-18 06:42:04 -0700

    More tasks on C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main-A program that puts line with puts function)
- *Return:0(Successful if it returns to zero)
+ *main-A program that puts a line with puts function
+ *Return:0(SUCCESS if it returns to zero)
  */
-int main(void)
+main()
 {
 puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
