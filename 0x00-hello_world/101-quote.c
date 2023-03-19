55f04fe2c8bc8a8a8d0c15a2a4a055685e6a96f3
Le-Artiste5 <johnsonotobong2@gmail.com>
2023-03-19 06:55:48 -0700

    More tasks on write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  *main-print quote
- *Description: Prints Dora korpar's quote
+ *Description: Prints Dora Korpar quote
  *Return: Always 1(SUCCESS)
  */
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
