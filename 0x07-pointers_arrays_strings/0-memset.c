a671fd2df20b2889d8f94d217ae9340d7c5f45fb
Le-Artiste5 <johnsonotobong2@gmail.com>
2023-04-05 01:28:06 -0700

    Memory set and is given a specific value

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,22 @@
 #include "main.h"
 /**
- *_memset- fill block of memory with a value
+ *_memset - fill a block of memory with a specific value
  *@s: starting address of memory to be filled
- *@b: Desired value
+ *@b: the desired value
  *@n: number of bytes to be changed
  *
- *Return: Change array with new value for n bytes
+ *Return: changed array with new value for n bytes
  */
-char *_memset(char *s, char b, unsigned int)
+char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0;
-for (; n > 0; i++)
-{
-s[i] = b;
-n--;
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
-return (s);
-}
+
+
