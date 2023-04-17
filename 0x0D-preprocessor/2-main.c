08ffae2f4450c30a4665d75bd0419e4ebadbec8a
Le-Artiste5 <johnsonotobong2@gmail.com>
2023-04-17 13:53:57 -0700

    Tasks on c preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Program writes a string as the name of file
+ * Return: Always return 0 (success)
+ *
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
+
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#defne FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif
+
