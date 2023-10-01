e9d87851ade0a9a110c627aaa216eda06b491645
Le-Artiste5 <johnsonotobong2@gmail.com>
2023-10-01 02:31:00 -0700

    Printing string on the screen

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - recursive function to print character on screen
+ * @s: points to the string
+ *
+ * Return: null
+ *
+ */
+
+void _puts_recursion(char *s)
+
+
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
