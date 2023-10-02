1e96dc7723a5cf048944ab80ee16de1ca0c36a25
Le-Artiste <johnsonotobong2@gmail.com>
2023-10-02 15:22:54 -0700

    A number that prints a string and a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ * main - function that prints name and new line
+ *
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+The command line arguements projects ARGV and ARGC
