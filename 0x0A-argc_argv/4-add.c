0fc2ed83ae4457f771a31ad54574a5e25e50a8ff
Le-Artiste <johnsonotobong2@gmail.com>
2023-10-02 17:05:32 -0700

    A program that adds different numbers and returns o if it is a symbol

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - A program that adds positive numbers
+ *
+ * @argc: argument counter
+ * @argv: argent vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b, sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for  (b  = 0; argv[a][b] != '\0'; b++)
+		if (isdigit(argv[a][b]))
+			{
+			       sum += atoi(argv[a]);
+				printf("%d\n", sum);
+				return (0);
+			}
+		else
+				printf("Error\n");
+	}
+}
