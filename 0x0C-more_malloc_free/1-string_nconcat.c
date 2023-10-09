2c055eb8256c62c7fd05ae154ded50e6caf30bed
Le-Artiste <johnsonotobong2@gmail.com>
2023-10-08 20:40:35 -0700

    Reallocates memory from old to new string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,35 +11,36 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-   size_t a, b, c, d;
+	size_t a, b, c;
+	char *q;
 
-   char *q;
-   if (s2 == NULL) {
-      b = 0;
-   }
-   else
-{
-   for (b = 0; s2[b] != '\0'; b++)
-}
-   if(s1 == NULL)
-     a = 0
-   else
-   for (a = 0; s1[a] != '\0'; a++)
+	if (s2 == NULL)
 
-   if (b > n)
-     b = n;
+	b = 0;
 
-   q = malloc(sizeof(char) * (a + b +1));
+	else
+	for (b = 0; s2[b] != '\0'; b++)
 
-    if (q == NULL)
-   return (NULL);
-    for (c = 0; c < a; c++)
-   q[c] = s1[c];
+	if (s1 == NULL)
 
-    for (c = 0; c < b; c++)
-    q[c + a] = s2[c];
-    q[a + b] = '\0';
-    return (q);
+	a = 0;
 
+	else
+	for (a = 0; s1[a] != '\0'; a++)
+	if (b > n)
+		b = n;
+
+	q = malloc(sizeof(char) * (a + b + 1));
+
+	if (q == NULL)
+	return (NULL);
+
+	for (c = 0; c < a; c++)
+	q[c] = s1[c];
+	for (c = 0; c < b; c++)
+	q[c + a] = s2[c];
+
+	q[a + b] = '\0';
+	return (q);
 }
 
