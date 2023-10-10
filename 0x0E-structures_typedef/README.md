fd40f1570339c2c3bdec91966443bd0dda1e8b87
Le-Artiste <johnsonotobong2@gmail.com>
2023-10-10 16:42:17 -0700

    Creating a struct and defining it

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This project deals with C structures and type definitions
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog- Defining a struct
+ * @name: name to be inputted
+ * @age: age
+ * @owner: Owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
