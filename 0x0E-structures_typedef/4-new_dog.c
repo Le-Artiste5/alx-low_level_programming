18834f07035e084a066d5d9f415f6d0a39905c5d
Le-Artiste <johnsonotobong2@gmail.com>
2023-10-10 19:59:17 -0700

    More tasks on typedef and structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,41 +12,40 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	size_t a,b,c;
+	size_t a, b, c;
 
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 	{
-                free(dog);
-                return (NULL);
-        }
-        if (name == NULL || owner == NULL)
-                return (NULL);
-        dog = malloc(sizeof(dog_t));
-        for (a = 0; name[a] != '\0'; a++)
-                ;
-        a++;
-        dog->name = malloc(sizeof(char) * a);
-        if (dog->name == NULL)
-        {
-                free(dog->name);
-                free(dog);
-                return (NULL);
-        }
-        for (c = 0; c < a; c++)
-                dog->name[c] = name[c];
- 
-        dog->age = age;
-        for (b = 0; owner[b] != '\0'; b++)
-                ;
-        b++;
-        dog->owner = malloc(sizeof(char) * b);
-        if (dog->owner == NULL)
-        {
-                free(dog->owner);
-                free(dog);
-                return (NULL);
-        }
-        for (c = 0; c < b; c++)
-                dog->owner[c] = owner[c];
+		free(dog);
+		return (NULL);
+	}
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	for (a = 0; name[a] != '\0'; a++)
+		;
+	a++;
+	dog->name = malloc(sizeof(char) * a);
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (c = 0; c < a; c++)
+	dog->name[c] = name[c];
+	dog->age = age;
+	for (b = 0; owner[b] != '\0'; b++)
+		;
+	b++;
+	dog->owner = malloc(sizeof(char) * b);
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		free(dog);
+		return (NULL);
+	}
+	for (c = 0; c < b; c++)
+		dog->owner[c] = owner[c];
 	return (dog);
 }
