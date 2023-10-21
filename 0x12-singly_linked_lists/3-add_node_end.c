e8bf9c2ac12a4a347fcad970a20785e8e27d85ed
Le-Artiste <johnsonotobong2@gmail.com>
2023-10-20 19:12:02 -0700

    Singly linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	n_node = *head;
 
-	temp = (list_t *)malloc(sizeof(list_t));
+	temp = malloc(sizeof(list_t));
 
 	a = 0;
 	while (str[a] != '\0')
@@ -30,11 +30,15 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	temp->next = NULL;
 
-	while (n_node->next != NULL)
+	if (n_node == NULL)
+		*head = temp;
+	else
+	{
+		while (n_node->next != NULL)
 	{
 		n_node = n_node->next;
 	}
-
+	}
 	n_node->next = temp;
 	return (*head);
 
