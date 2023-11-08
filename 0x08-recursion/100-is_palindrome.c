494ec5c3f524f3317776723f79aaac18185a6d63
Le-Artiste <johnsonotobong2@gmail.com>
2023-11-08 00:07:40 -0800

    palindrome corrrection

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,38 +1,42 @@
 #include "main.h"
 /**
- * _strlen-recursion - helps get the string length
+ * _strlen_recursion - helps get the string length
  * @s: string of characters
  *
  * Return: The length of the inputed string
- * 
+ *
  */
 int _strlen_recursion(char *s)
-{       
-        if (*s == '\0')
-                return (0);
-        else
-        return (1 + _strlen_recursion(s + 1));
-}       
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
 
 /**
- *comp_string - compares characters in the string
- *@s: The string ton be checked
- *@s_iterator: least iterator
+ * com_string - compares characters in the string
+ * @s: The string ton be checked
+ * @s_iterator: least iterator
  *
- *@l_iterator: largest iterator
+ * @l_iterator: largest iterator
  *
  * Return: returns an integer
  */
 int com_string(char *s, int s_iterator, int l_iterator)
-{       
-        if ((s + s_iterator) == (s + l_iterator))
+{
+	if (*(s + s_iterator) == *(s + l_iterator))
+	{
 		if (s_iterator == l_iterator || s_iterator == l_iterator + 1)
 			return (1);
 	return (0 + com_string(s, s_iterator + 1, l_iterator - 1));
+	}
+
+	return (0);
+
 }
 
 /**
- *
  * is_palindrome - checks if a string is a palindrome
  * @s: The string to be checked
  *
@@ -44,7 +48,5 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return (com_string(s, 0, _strlen_recursion(s - 1)));
+	return (com_string(s, 0, _strlen_recursion(s) - 1));
 }
-                                                                                                                                                                                
-                            
